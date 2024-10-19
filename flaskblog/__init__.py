3bb396f0ae067904ea9df973b6fae2fec1720644
meenu <meenu10802@gmail.com>
2024-10-20 00:07:06 +0530

    added database

---
+++ flaskblog/__init__.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your_secret_key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+from flaskblog import routes
+
+with app.app_context():
+    db.create_all()
